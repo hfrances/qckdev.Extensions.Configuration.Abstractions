10702d52c3b9edf67e12fc4bab097bbeaeabab9a
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2022-08-31 15:34:38 +0200

    fix: namespace

--- qckdev.Extensions.Configuration.Abstractions/ConfigurationHelper.cs
+++ qckdev.Extensions.Configuration.Abstractions/ConfigurationHelper.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace qckdev.Extensions.Configuration.Abstractions
+namespace qckdev.Extensions.Configuration
 {
 
     /// <summary>
